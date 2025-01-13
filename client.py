3d20e3ccb41353d6633a4f60ac48d9c437231c2f
immensesenator4 <Wizkid9955@gmail.com>
2025-01-12 20:19:05 -0700

    new client class hastily made

--- client.py
+++ client.py
@@ -5,15 +5,20 @@ import socket
 #this will continue with a self.host and self. ip adress which i would have to make somethingto search the port i think
 HOST =  "192.168.12.195" 
 PORT = 13455 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"l=Hello, world")
-    data = s.recv(1024)
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:   
-    s.connect((HOST, PORT)) 
-    s.sendall(b"l= recieve")
-    data = s.recv(1024)
-    
-print(f"Received {data!r}")
+class client(object):
+    def __init__(self,host,port):
+        self.host=host
+        self.port = port
+    def send(self,var,contents):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            s.sendall(f"{var}={contents}".encode())
+            return s.recv(1024)
+    def recieve(self,var):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:   
+            s.connect((self.host, self.port)) 
+            s.sendall(f"{var}=recieve".encode())
+            return s.recv(1024)
+d=client("192.168.12.195",13455 )
+d.send("player","with trust i am you to say\n hello new world")
+print(f"Received {d.recieve("player")!r}")
