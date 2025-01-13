30e83889cf692a74b16857e02d68e01fa738705c
immensesenator4 <Wizkid9955@gmail.com>
2025-01-12 18:34:16 -0700

    true conect rebar vers fowler will get

--- client.py
+++ client.py
@@ -1,17 +1,19 @@
 import socket
-
-HOST =  "192.168.12.195" # The server's hostname or IP address
-PORT = 13455  # The port used by the server
+#type will be stored in the future so we can transmute integers and booleans and lists
+# this will happen when client class is made
+#what is below is a send and recieve function
+#this will continue with a self.host and self. ip adress which i would have to make somethingto search the port i think
+HOST =  "192.168.12.195" 
+PORT = 13455 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"l=Hello, world")
     data = s.recv(1024)
-print(f" {data!r}")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:   
     s.connect((HOST, PORT)) 
     s.sendall(b"l= recieve")
     data = s.recv(1024)
     
-
 print(f"Received {data!r}")
--- host.py
+++ host.py
@@ -2,11 +2,8 @@ import socket
 class Host(object):
     def __init__(self,size:int,port:int):
             self.port=port
-            self.s = socket.socket()
             self.hostname = socket.gethostname()
-            self.ip_address = socket.gethostbyname(self.hostname)
-            print(self.ip_address)
-            
+            self.ip_address = socket.gethostbyname(self.hostname)            
             self.data={}
             self.var=[]
     def get_person(self):
@@ -15,14 +12,12 @@ class Host(object):
                 s.listen()
                 conn, addr = s.accept()
                 with conn:
-                    print(f"Connected by {addr}")
                     while True:
                         data = conn.recv(1024)
                         if not data:
                             break
                         if ("recieve" in f"{data!r}" ):
-                            l=""
-                            print(True)
+                            var=""
                             count=0
                             for i in f"{data!r}":
                                 if count==0:
@@ -30,17 +25,16 @@ class Host(object):
                                 elif i == "=":
                                      break
                                 else:
-                                     l+=i
+                                     var+=i
                                 count+=1
-                            print("l")
                             
                                  
-                            if l in self.var:
-                                self.send(self.data[l],conn)
+                            if var in self.var:
+                                self.send(self.data[var],conn)
                             else:
                                  self.send(b'N/a',conn)
                         else:
-                            l=""
+                            var=""
                             count=0
                             for i in f"{data!r}":
                                 if count==0:
@@ -48,17 +42,16 @@ class Host(object):
                                 elif i == "=":
                                      break
                                 else:
-                                     l+=i
+                                     var+=i
                                 count+=1
-                            self.var.append(l)
-                            print(l)
-                            count = len(l)+1
+                            self.var.append(var)
+                            count = len(var)+1
                             new_data=""
                             for char in f"{data!r}":
                                  if count<0:
                                       new_data+=char
                                  count-=1
-                            self.data[l]= new_data.encode()
+                            self.data[var]= new_data.encode()
                             self.send(b'recieved',conn)
                         return data,addr
     def send(self,data:bytes,conn):
